8d25c9feca1731d2042a669fe180f046dba7826c
Nguyen Tien Dung Otten <nguyen.tiendung.otten@gmail.com>
2020-07-16 20:11:23 +0200

    Improvements

--- api/run_api.py
+++ api/run_api.py
@@ -23,13 +23,86 @@ from sklearn.impute import IterativeImputer
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
-from sklearn.feature_selection import SelectKBest, f_regression, chi2
+from sklearn.feature_selection import SelectKBest, f_regression, chi2, f_classif
 
 
 UPLOAD_FOLDER = '/path/to/the/uploads'
     
-def feature_selection_transformation(X, y, target, ordinal_feature):
-    # check for numeric features train_data
+def regress_feature_selection_transformation(X, y, target, ordinal_feature, route):
+    
+    def train_num_feature_selection(X_num):
+        # saleprice correlation matrix
+        k_num = round(len(X_num.columns) / 2)
+        corrmat = X_num.corr()
+        X_num_fs = corrmat.nlargest(k_num, target)[target].index
+    
+        # check for multicollinearity
+        # if two features are strongly correlated with each other (>= 0.7) 
+        # the feature with the lower correlation with the target variable is dropped
+        multicorr = {}
+        k = len(corrmat)
+        for feature in corrmat:
+            i = 1
+            if feature != target:
+                while i < k - 1:
+                    if corrmat[feature][i] >= 0.7 and feature != corrmat.index[i]:
+                        multicorr[feature] = corrmat.index[i], corrmat[feature][i]
+                    i = i + 1
+        
+        # delete duplicates
+        corr_scores = []
+        for feature in list(multicorr.keys()):
+            if multicorr[feature][1] in corr_scores:
+                del multicorr[feature]
+            else:
+                corr_scores.append(multicorr[feature][1])
+        
+        dropped_features = []
+        # remove the feature with the lower correlation coefficient (pearson) 
+        for feature1, feature2 in multicorr.items():
+            if corrmat[target][feature1] < corrmat[target][feature2[0]]:
+                dropped_features.append(feature1)
+            else:
+                dropped_features.append(feature2[0])
+    
+        # drop the features from X_num dataframe
+        for feature in X_num:
+            if feature in dropped_features:
+                X_num = X_num.drop(feature, axis = 1)
+        drop_corr_features = X_num.columns.difference(X_num_fs)
+        X_num.drop(X_num.columns.difference(X_num_fs), 1, inplace = True)
+        
+        return (X_num, dropped_features, drop_corr_features)
+    
+    def train_cat_feature_selection(X_cat, X_cat_enc):
+        # feature selection on categorical data
+        k_cat = round(len(X_cat.columns) / 2)
+        fs = SelectKBest(f_classif, k_cat)
+        fs.fit(X_cat_enc, y) # save!!
+        X_cat_fs = fs.transform(X_cat_enc)
+        X_cat_enc = pd.DataFrame(X_cat_fs)
+        
+        return (X_cat_enc, fs)
+    
+    def predict_num_feature_selection(X_num):
+        with open('fs_values.pkl', 'rb') as file:
+            drop_corr_features, drop_multicoll_features = pickle.load(file)[:2]
+            
+        X_num = X_num.drop(drop_multicoll_features, axis = 1) 
+        X_num.drop(drop_corr_features, 1, inplace = True)
+        
+        return X_num
+    
+    def predict_cat_feature_selection(X_cat_enc):
+        with open('fs_values.pkl', 'rb') as file:
+            selected_cat_features, dummy = pickle.load(file)[2:4]
+            
+        X_cat_fs = selected_cat_features.transform(X_cat_enc)
+        X_cat_enc = pd.DataFrame(X_cat_fs)
+        
+        return X_cat_enc
+    
+    # split features
     num_features = []
     cat_features = []
     for feature in X:
@@ -39,84 +112,164 @@ def feature_selection_transformation(X, y, target, ordinal_feature):
             cat_features.append(feature)
     
     X = X.reset_index()
+
     # impute using only numerical features
-    imp = IterativeImputer(max_iter = 10, random_state = 42)
-    imp.fit(X[num_features])
-    X[num_features] = imp.transform(X[num_features])
+    if num_features:
+        imp = IterativeImputer(max_iter = 10, random_state = 42)
+        imp.fit(X[num_features])
+        X[num_features] = imp.transform(X[num_features])
+        X_num = X.drop(cat_features, axis = 1)
     
     # impute using only categorical features
-    imp = SimpleImputer(missing_values = np.nan, strategy = 'most_frequent')
-    X[cat_features] = imp.fit_transform(X[cat_features].astype(str))
-     
-    # split dataframe into numeric and categorical data
-    X_num = X.drop(cat_features, axis = 1)
-    X_cat = X.drop(num_features, axis = 1)
-    
-    # saleprice correlation matrix
-    k_num = round(len(X_num.columns) / 2)
-    corrmat = X_num.corr()
-    X_num_fs = corrmat.nlargest(k_num, target)[target].index
-    
-    # check for multicollinearity
-    # if two features are strongly correlated with each other (>= 0.7) 
-    # the feature with the lower correlation with the target variable is dropped
-    multicorr = {}
-    k = len(corrmat)
-    for feature in corrmat:
-        i = 1
-        if feature != target:
-            while i < k - 1:
-                if corrmat[feature][i] >= 0.7 and feature != corrmat.index[i]:
-                    multicorr[feature] = corrmat.index[i], corrmat[feature][i]
-                i = i + 1
+    if cat_features:
+        imp = SimpleImputer(missing_values = np.nan, strategy = 'most_frequent')
+        X[cat_features] = imp.fit_transform(X[cat_features].astype(str))
+        X_cat = X.drop(num_features, axis = 1)
         
-    # delete duplicates
-    corr_scores = []
-    for feature in list(multicorr.keys()):
-        if multicorr[feature][1] in corr_scores:
-            del multicorr[feature]
+    # get column count
+    if num_features:
+        num_shape = X_num.shape[1]
+    
+    if cat_features:
+        cat_shape = X_cat.shape[1]
+    
+    # feature selection numerical features
+    if route == '/train':
+        if num_features and not num_shape <= 10:
+            X_num, drop_multicoll_features, drop_corr_features = train_num_feature_selection(X_num)
         else:
-            corr_scores.append(multicorr[feature][1])
-            
-    # remove the feature with the lower correlation coefficient (pearson)
-    dropped_features = [] 
-    for feature1, feature2 in multicorr.items():
-        if corrmat[target][feature1] < corrmat[target][feature2[0]]:
-            dropped_features.append(feature1)
-        else:
-            dropped_features.append(feature2[0])
+            drop_multicoll_features = []
+            drop_corr_features = []
+    elif route == '/predict':
+        if num_features and not num_shape <= 10:
+            X_num = predict_num_feature_selection(X_num)
+        
+    # encode ordinal features (dummy variables)
+    if ordinal_feature is not None and ordinal_feature in X_num:
+        ord_data = [ordinal_feature]
+        X_num = pd.get_dummies(X_num, columns = ord_data, drop_first = True)
 
-    # drop the features from X_num dataframe
-    for feature in X_num:
-        if feature in dropped_features:
-            X_num = X_num.drop(feature, axis = 1) 
-    X_num.drop(X_num.columns.difference(X_num_fs), 1, inplace = True)
+    # encode categorical features
+    if cat_features:
+        enc = OrdinalEncoder()
+        enc.fit(X_cat)
+        X_cat_enc = enc.transform(X_cat)
+    
+    # feature selection caterorical features
+    if route == '/train':
+        if cat_features and not cat_shape <= 10:
+            X_cat_enc, selected_cat_features = train_cat_feature_selection(X_cat, X_cat_enc)
+        else:
+            selected_cat_features = None
+    elif route == '/predict':
+        if cat_features and not cat_shape <= 10:
+            X_cat_enc = predict_cat_feature_selection(X_cat_enc)
+    
+    # concatenate numerical and categorical features
+    if cat_features and num_features:
+        df_cat = pd.DataFrame(X_cat_enc, index = list(range(len(X.index))))
+        df_num = pd.DataFrame(X_num, index = list(range(len(X.index))))
+        X = pd.concat([df_cat, df_num], axis = 1, sort = False)
+        if route == '/train':
+            X = X.drop([target], axis = 1)
+    elif cat_features:
+        X = pd.DataFrame(X_cat_enc)
+    elif num_features:
+        X = pd.DataFrame(X_num)
+    
+    if route == '/train':
+        # serialize feature selection values
+        fs_values = [drop_corr_features, drop_multicoll_features, selected_cat_features, ordinal_feature, target]
+        with open('fs_values.pkl', 'wb') as file:
+            pickle.dump(fs_values, file)
+        
+    return X
+
+def class_feature_selection_transformation(X, y, target, ordinal_feature, route):
+    
+    def num_feature_selection(X_num):
+        # feature selection on numerical data
+        k_num = round(len(X_num.columns) / 2)
+        fs = SelectKBest(f_classif, k_num)
+        fs.fit(X_num, y) # save!!
+        X_num_fs = fs.transform(X_num)
+        return X_num_fs
+        
+    def cat_feature_selection(X_cat, X_cat_enc):
+        # feature selection on categorical data
+        k_cat = round(len(X_cat.columns) / 2)
+        fs = SelectKBest(chi2, k_cat)
+        fs.fit(X_cat_enc, y) # save!!
+        X_cat_fs = fs.transform(X_cat_enc)
+        X_cat_enc = pd.DataFrame(X_cat_fs)
+        return X_cat_enc
+    
+    # split features
+    num_features = []
+    cat_features = []
+    for feature in X:
+        if X[feature].dtypes == np.int or X[feature].dtypes == np.float:
+            num_features.append(feature)
+        else:
+            cat_features.append(feature)
+    
+    X = X.reset_index()
+    
+    # impute using only numerical features
+    if num_features:
+        imp = IterativeImputer(max_iter = 10, random_state = 42)
+        imp.fit(X[num_features])
+        X[num_features] = imp.transform(X[num_features])
+        X_num = X.drop(cat_features, axis = 1)
+    
+    # impute using only categorical features
+    if cat_features:
+        imp = SimpleImputer(missing_values = np.nan, strategy = 'most_frequent')
+        X[cat_features] = imp.fit_transform(X[cat_features].astype(str))
+        X_cat = X.drop(num_features, axis = 1)
+        
+    # get column count
+    if num_features:
+        num_shape = X_num.shape[1]
+    
+    if cat_features:
+        cat_shape = X_cat.shape[1]
+    
+    # drop target
+    if target in X_num:
+        X_num = X_num.drop(target, axis = 1)
+    if target in X_cat:
+        X_cat = X_cat.drop(target, axis = 1)
+    
+    if num_features and not num_shape <= 10:
+        X_num = num_feature_selection(X_num)
     
     # encode ordinal features (dummy variables)
-    if ordinal_feature in X_num:
+    if ordinal_feature is not None:
         ord_data = [ordinal_feature]
         X_num = pd.get_dummies(X_num, columns = ord_data, drop_first = True)
     
-    # encode categorical features
-    enc = OrdinalEncoder()
-    enc.fit(X_cat)
-    X_cat_enc = enc.transform(X_cat)
+    if cat_features:
+        enc = OrdinalEncoder()
+        enc.fit(X_cat)
+        X_cat_enc = enc.transform(X_cat)
     
-    # feature selection on categorical data
-    k_cat = round(len(X_cat.columns) / 2)
-    fs = SelectKBest(f_regression, k_cat)
-    fs.fit(X_cat_enc, y) # save!!
-    X_cat_fs = fs.transform(X_cat_enc)
-    X_cat_enc = pd.DataFrame(X_cat_fs)
+    if cat_features and not cat_shape <= 10:
+        X_cat_enc = cat_feature_selection(X_cat, X_cat_enc)
     
     # concatenate numerical and categorical features
-    df_cat = pd.DataFrame(X_cat_enc, index = list(range(len(X.index))))
-    df_num = pd.DataFrame(X_num, index = list(range(len(X.index))))
-    X = pd.concat([df_cat, df_num], axis = 1, sort = False)
-    X = X.drop([target], axis = 1)
+    if num_features and cat_features:
+        df_cat = pd.DataFrame(X_cat_enc, index = list(range(len(X.index))))
+        df_num = pd.DataFrame(X_num, index = list(range(len(X.index))))
+        X = pd.concat([df_cat, df_num], axis = 1, sort = False)
+    elif cat_features:
+        X = pd.DataFrame(X_cat_enc)
+    elif num_features:
+        X = pd.DataFrame(X_num)
+    
     return X
 
-def predict_randomforestclass(X, y, path):
+def predict_randomforestclass(X, y):
     # build model
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
     
@@ -128,13 +281,13 @@ def predict_randomforestclass(X, y, path):
     acc_score = accuracy_score(y_test, y_predict)
     
     # save model on disk
-    with open(path, "wb") as file:
+    with open('model.pkl', "wb") as file:
         pickle.dump(rf, file)
     
     ret_stmt = 'Accuracy Score: ' + str(acc_score)
     return ret_stmt
 
-def predict_randomforestregress(X, y, path):
+def predict_randomforestregress(X, y):
     # build model
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
     
@@ -150,7 +303,7 @@ def predict_randomforestregress(X, y, path):
     ret_stmt = 'R^2 Score: ' + str(r_2_score) + '\n' + 'RMSLE: ' + str(rmsle)
     
     # save model on disk
-    with open(path, "wb") as file:
+    with open('model.pkl', "wb") as file:
         pickle.dump(rf, file)
 
     return ret_stmt
@@ -159,8 +312,9 @@ def predict_randomforestregress(X, y, path):
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
     
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/train', methods=['POST'])
+def train():
+    route = request.path
     # retrieve data
     data = request.get_json(force = True)
     train_data = pd.DataFrame(data)
@@ -170,7 +324,6 @@ def predict():
     del_feature = request.args.get('del_feature')
     ordinal_feature = request.args.get('ordinal_feature')
     model_type = request.args.get('model_type')
-    path = request.args['path']
     
     # select features and target variable
     features = list(train_data)
@@ -181,20 +334,45 @@ def predict():
     if del_feature is not None:
         X = X.drop(del_feature, axis = 1) 
     
-    feature_engineering = feature_selection_transformation(X, y, target, ordinal_feature)
     if model_type is None:
         if train_data[target].dtypes == np.object:
-            prediction = predict_randomforestclass(feature_engineering, y, path)
+            feature_engineering = class_feature_selection_transformation(X, y, target, ordinal_feature, route)
+            prediction = predict_randomforestclass(feature_engineering, y)
             return prediction
         elif train_data[target].dtypes == np.float or train_data[target].dtypes == np.int:
-            prediction = predict_randomforestregress(feature_engineering, y, path)
+            feature_engineering = regress_feature_selection_transformation(X, y, target, ordinal_feature, route)
+            prediction = predict_randomforestregress(feature_engineering, y)
             return prediction
     elif model_type == 'classifier':
-        prediction = predict_randomforestclass(feature_engineering, y, path)
+        feature_engineering = class_feature_selection_transformation(X, y, target, ordinal_feature, route)
+        prediction = predict_randomforestclass(feature_engineering, y)
         return prediction
     elif model_type == 'regressor':
-        prediction = predict_randomforestregress(feature_engineering, y, path)
+        feature_engineering = regress_feature_selection_transformation(X, y, target, ordinal_feature, route)
+        prediction = predict_randomforestregress(feature_engineering, y)
         return prediction
+    
+@app.route('/predict', methods=['POST'])
+def predict():
+   route = request.path
+   # retrieve data
+   data = request.get_json(force = True)
+   test_data = pd.DataFrame(data)
+
+   with open('fs_values.pkl', 'rb') as file:
+        ordinal_feature, target = pickle.load(file)[3:]
+   with open('model.pkl', 'rb') as file:
+        model = pickle.load(file)
+
+   feature_engineering = regress_feature_selection_transformation(test_data, None, None, ordinal_feature, route)
+   y_predict = model.predict(feature_engineering)
+    
+   df_features = pd.DataFrame(test_data)
+   df_prediction = pd.DataFrame({target: y_predict})
+   output = pd.concat([df_features, df_prediction], axis = 1, sort = False)
+   output.to_csv('prediction.csv', index = False)
+
+   return "Successful"
     
 app.run(debug=True)
                                 
